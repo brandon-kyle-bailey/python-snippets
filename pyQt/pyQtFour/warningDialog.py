ca7bff1a60f652c4c773e33cf4342bfa247fbff0
Brandon <39861127+brandon-kyle-bailey@users.noreply.github.com>
2020-02-14 18:42:02 +0000

    Create warningDialog.py

---
+++ pyQt/pyQtFour/warningDialog.py
@@ -0,0 +1,44 @@
+#/usr/bin/env python
+
+def query_some_data():
+    return {"name":1, "name":2, "name":3}
+
+
+def raise_warning_dialog(QtGui):
+
+    conflict_msg = "This is a warning"
+
+    reply = QtGui.QMessageBox.question(QtGui.QMainWindow(),
+                                       'Conflict found!',
+                                        conflict_msg,
+                                        QtGui.QMessageBox.YesToAll,
+                                        QtGui.QMessageBox.Yes,
+                                        QtGui.QMessageBox.No)
+
+    return reply
+
+
+def test():
+
+    from PyQt4 import QtGui
+    app = QtGui.QApplication([__name__])
+
+    values_to_match = [1, 2, 3]
+
+    results = query_some_data()
+    used_names = {val for name, val in results.iteritems()}
+
+    for name in values_to_match:
+        if name in used_names:
+            reply = raise_warning_dialog(QtGui)
+
+            if reply == QtGui.QMessageBox.YesToAll:
+                break
+            elif reply == QtGui.QMessageBox.Yes:
+                continue
+            else:
+                raise ValueError("This is a value error")
+
+
+if __name__ == "__main__":
+    test()
